54d3284c723a273c6d79e319cd30daa34b172ab2
Ryan Anguiano <ryan.anguiano@gmail.com>
2017-05-07 22:09:55 -0400

    Update docker-compose and requirements

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,13 @@
-FROM python:2.7-slim
+FROM confluentinc/cp-base
 
 MAINTAINER Ryan Anguiano
-
-# Install librdkafka from Confluent Repo
-ADD http://packages.confluent.io/deb/3.2/archive.key /confluent.key
-RUN apt-key add /confluent.key && \
-    echo "deb [arch=amd64] http://packages.confluent.io/deb/3.2 stable main" >> /etc/apt/sources.list && \
-    apt-get update && \
-    apt-get install -y librdkafka-dev confluent-kafka-2.11 --no-install-recommends && \
-    rm -rf /var/lib/apt/lists/* && \
-    rm /confluent.key
 
 WORKDIR /app
 ADD requirements.txt requirements_dev.txt ./
 
 RUN set -ex && \
     buildDeps='build-essential curl gcc git make' && \
-    projectDeps='libpcre3-dev libssl-dev libffi-dev' && \
+    projectDeps='libpcre3-dev libssl-dev libffi-dev python-dev librdkafka-dev confluent-kafka-2.11' && \
     apt-get update && \
     apt-get install -y $buildDeps $projectDeps --no-install-recommends && \
     pip install -U pip setuptools wheel && \
--- tests/test_kafka_gconsumer.py
+++ tests/test_kafka_gconsumer.py
@@ -6,20 +6,63 @@ test_kafka_gconsumer
 Tests for `kafka_gconsumer` module.
 """
 
+import os
 import pytest
+import random
+import string
 
+from confluent_kafka import Producer
+from confluent_kafka.avro import AvroProducer
 from kafka_gconsumer import Consumer, AvroConsumer
 
 
-@pytest.fixture(scope='session')
-def produced_plain_messages():
+def random_str(length):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length))
+
+
+@pytest.fixture
+def consumer_settings():
+    return {
+        'bootstrap.servers': os.environ.get('KAFKA_BOOTSTRAP_SERVERS'),
+        'group.id': 'gConsumer-test-consumer-{}'.format(random_str(5)),
+        'topic.auto.offset.reset': 'earliest',
+    }
+
+
+@pytest.fixture
+def producer_settings():
+    return {
+        'bootstrap.servers': os.environ.get('KAFKA_BOOTSTRAP_SERVERS'),
+        'group.id': 'gConsumer-test-producer-{}'.format(random_str(5)),
+    }
+
+
+@pytest.fixture
+def avro_consumer_settings(consumer_settings):
+    settings = {
+        'schema.registry.url': os.environ.get('KAFKA_SCHEMA_REGISTRY_URL'),
+    }
+    settings.update(consumer_settings)
+    return settings
+
+
+@pytest.fixture
+def avro_producer_settings(producer_settings):
+    settings = {
+        'schema.registry.url': os.environ.get('KAFKA_SCHEMA_REGISTRY_URL'),
+    }
+    settings.update(producer_settings)
+    return settings
+
+
+def produced_plain_messages(topic, count):
     pass
 
 
-@pytest.fixture(scope='session')
-def produced_avro_messages():
+def produced_avro_messages(topic):
     pass
 
 
-def test_content(produced_plain_messages):
-    pass
+def test_producer(producer_settings):
+    producer = Producer(**producer_settings)
+    producer.produce('test_messages', 'test123')
